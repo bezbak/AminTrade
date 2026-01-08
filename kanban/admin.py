3e0998c3e0506ad38f5b05be4845507a5848d2a0
bezbak <Cemetey.2019@gmail.com>
2026-01-08 17:54:33 +0600

    faesf

--- kanban/admin.py
+++ kanban/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Container, Vehicle, Contact, WhatsAppAccount, PipelineStage, Messages
+from .models import Container, Vehicle, Contact, WhatsAppAccount, PipelineStage, Messages, CallBack
 # Register your models here.
 admin.site.register(Container)
 admin.site.register(Vehicle)
 admin.site.register(Contact)
 admin.site.register(WhatsAppAccount)
 admin.site.register(PipelineStage)
+admin.site.register(CallBack)
 admin.site.register(Messages)
